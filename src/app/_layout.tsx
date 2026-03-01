9562b46ad222e84a39f0210df86a3e47177ee458
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-01 04:21:25 +0100

    add ThemeProvider in root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,13 @@
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from "expo-router";
 import '../../global.css';
 
 export default function RootLayout() {
     return (
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "Events" }} />
-      </Stack>
+      <ThemeProvider value={DarkTheme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ title: "Events" }} />
+        </Stack>  
+      </ThemeProvider>
     )
 }
