e1709a675e4f44bd436db4c6c84bc31bd67b3dc4
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-02 16:47:21 +0100

    add simple camera screen

---
+++ src/app/camera.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Camera() {
+  return (
+    <View className="flex-1 justify-center items-center bg-red-200">
+      <Text className="text-3xl font-bold text-white">Camera</Text>
+    </View>
+  );
+}
