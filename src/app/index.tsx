8941762aa98879533706cf6ca9a347134efebb39
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-02 16:47:38 +0100

    add a link to open camera screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center bg-red-200">
+    <View className="flex-1 justify-center items-center">
       <Text className="text-3xl font-bold text-white">Home</Text>
+      <Link href="/camera" className="text-white">Open Camera</Link>
     </View>
   );
 }
