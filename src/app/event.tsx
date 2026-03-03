c1af7274c8078a70d826250d992e86dd424109b1
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-03 23:57:38 +0100

    add simple event screen

---
+++ src/app/event.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Event() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl font-bold text-white">Event</Text>
+    </View>
+  );
+}
