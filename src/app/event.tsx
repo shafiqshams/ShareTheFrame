b6cfc21d3b61895b95ace215d70e4715b7956e5b
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-04 23:59:06 +0100

    add advance image to display photo

--- src/app/event.tsx
+++ src/app/event.tsx
@@ -1,9 +1,14 @@
+import { AdvancedImage } from "cloudinary-react-native";
 import { Text, View } from "react-native";
+import { cloudinary } from "../lib/cloudinary";
 
 export default function Event() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-3xl font-bold text-white">Event</Text>
+      <AdvancedImage 
+        cldImg={cloudinary.image('gtzjmtrte3v6xg9zrjks')} 
+        className="w-full aspect-[3/4]"/>
     </View>
   );
 }
