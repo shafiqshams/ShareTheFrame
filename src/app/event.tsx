18b1bbc946f3877b8a02e414760ca0f81ad16747
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-05 23:59:05 +0100

    add image resize using thumbnail

--- src/app/event.tsx
+++ src/app/event.tsx
@@ -1,13 +1,25 @@
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
 import { AdvancedImage } from "cloudinary-react-native";
-import { Text, View } from "react-native";
+import { PixelRatio, Text, useWindowDimensions, View } from "react-native";
 import { cloudinary } from "../lib/cloudinary";
 
 export default function Event() {
+  
+  const { width: windowWidth } = useWindowDimensions();
+  const pixelRatio = PixelRatio.get();
+
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-3xl font-bold text-white">Event</Text>
       <AdvancedImage 
-        cldImg={cloudinary.image('gtzjmtrte3v6xg9zrjks')} 
+        cldImg={
+          cloudinary.image('dx2t6nymuyymgyaea0hf')
+          .resize(
+            thumbnail()
+              .width(windowWidth * pixelRatio)
+              .height(windowWidth * pixelRatio)
+          )
+        } 
         className="w-full aspect-[3/4]"/>
     </View>
   );
