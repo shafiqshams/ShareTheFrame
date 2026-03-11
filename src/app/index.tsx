acfcb13b1b2f6cce2e4bb040ad12cdf682e0413a
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-11 02:45:56 +0100

    check supabase returns events data

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,23 @@
 import { Link } from "expo-router";
+import { useEffect } from "react";
 import { View } from "react-native";
+import { supabase } from "../lib/supabase";
 
 export default function Home() {
+  useEffect(() => {
+    supabase
+      .from("events")
+      .select("*")
+      .then((data) => console.log(JSON.stringify(data, null, 2)));
+  }, []);
   return (
     <View className="flex-1 justify-center items-center gap-4">
-      <Link href="/camera" className="text-white">Open Camera</Link>
-      <Link href="/event" className="text-white">Open Event</Link>
+      <Link href="/camera" className="text-white">
+        Open Camera
+      </Link>
+      <Link href="/event" className="text-white">
+        Open Event
+      </Link>
     </View>
   );
 }
